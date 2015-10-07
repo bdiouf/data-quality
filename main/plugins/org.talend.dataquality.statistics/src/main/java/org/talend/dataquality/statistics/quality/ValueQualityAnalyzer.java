3574efd4f2aca1fcfb92ccf8de4ef10be539a84d
Sizhao Liu <sizhaoliu@talend.com>
2015-10-07 18:57:54 +0200

    TDQ-10413 Compute the list of invalid values according to the semantic type

--- main/plugins/org.talend.dataquality.statistics/src/main/java/org/talend/dataquality/statistics/quality/ValueQualityAnalyzer.java
+++ main/plugins/org.talend.dataquality.statistics/src/main/java/org/talend/dataquality/statistics/quality/ValueQualityAnalyzer.java
@@ -12,13 +12,14 @@
 // ============================================================================
 package org.talend.dataquality.statistics.quality;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.NotImplementedException;
 import org.talend.datascience.common.inference.Analyzer;
-import org.talend.datascience.common.inference.ResizableList;
+import org.talend.datascience.common.inference.QualityAnalyzer;
+import org.talend.datascience.common.inference.ValueQualityStatistics;
 import org.talend.datascience.common.inference.type.DataType;
-import org.talend.datascience.common.inference.type.TypeInferenceUtils;
 
 /**
  * created by talend on 2015-07-28 Detailled comment.
@@ -28,27 +29,42 @@ public class ValueQualityAnalyzer implements Analyzer<ValueQualityStatistics> {
 
     private static final long serialVersionUID = -5951511723860660263L;
 
-    private final DataType.Type[] types;
+    private final QualityAnalyzer<ValueQualityStatistics, DataType.Type[]> dataTypeQualityAnalyzer;
 
-    private final ResizableList<ValueQualityStatistics> results = new ResizableList<>(ValueQualityStatistics.class);
+    private final QualityAnalyzer<ValueQualityStatistics, String[]> semanticQualityAnalyzer;
 
-    private boolean isStoreInvalidValues = true;
+    public ValueQualityAnalyzer(QualityAnalyzer<ValueQualityStatistics, DataType.Type[]> dataTypeQualityAnalyzer,
+            QualityAnalyzer<ValueQualityStatistics, String[]> semanticQualityAnalyzer, boolean isStoreInvalidValues) {
+        this.dataTypeQualityAnalyzer = dataTypeQualityAnalyzer;
+        this.semanticQualityAnalyzer = semanticQualityAnalyzer;
+        setStoreInvalidValues(isStoreInvalidValues);
+    }
+
+    public ValueQualityAnalyzer(QualityAnalyzer<ValueQualityStatistics, DataType.Type[]> dataTypeQualityAnalyzer,
+            QualityAnalyzer<ValueQualityStatistics, String[]> semanticQualityAnalyzer) {
+        this(dataTypeQualityAnalyzer, semanticQualityAnalyzer, true);
+    }
 
     public ValueQualityAnalyzer(DataType.Type[] types, boolean isStoreInvalidValues) {
-        this.isStoreInvalidValues = isStoreInvalidValues;
-        this.types = types;
+        this(new DataTypeQualityAnalyzer(types, isStoreInvalidValues), null, isStoreInvalidValues);
     }
 
     public ValueQualityAnalyzer(DataType.Type... types) {
-        this.types = types;
+        this(new DataTypeQualityAnalyzer(types), null);
     }
 
     public void init() {
-        // Nothing to do.
+        dataTypeQualityAnalyzer.init();
+        if (semanticQualityAnalyzer != null) {
+            semanticQualityAnalyzer.init();
+        }
     }
 
     public void setStoreInvalidValues(boolean isStoreInvalidValues) {
-        this.isStoreInvalidValues = isStoreInvalidValues;
+        dataTypeQualityAnalyzer.setStoreInvalidValues(isStoreInvalidValues);
+        if (semanticQualityAnalyzer != null) {
+            semanticQualityAnalyzer.setStoreInvalidValues(isStoreInvalidValues);
+        }
     }
 
     /**
@@ -63,29 +79,11 @@ public class ValueQualityAnalyzer implements Analyzer<ValueQualityStatistics> {
     }
 
     public boolean analyze(String... record) {
-        if (record == null) {
-            record = new String[] { StringUtils.EMPTY };
+        boolean status = this.dataTypeQualityAnalyzer.analyze(record);
+        if (status && this.semanticQualityAnalyzer != null) {
+            status = this.semanticQualityAnalyzer.analyze(record);
         }
-        results.resize(record.length);
-        for (int i = 0; i < record.length; i++) {
-            final String value = record[i];
-            final ValueQualityStatistics valueQuality = results.get(i);
-            if (TypeInferenceUtils.isEmpty(value)) {
-                valueQuality.incrementEmpty();
-            } else if (TypeInferenceUtils.isValid(types[i], value)) {
-                valueQuality.incrementValid();
-            } else {
-                valueQuality.incrementInvalid();
-                processInvalidValue(valueQuality, value);
-            }
-        }
-        return true;
-    }
-
-    private void processInvalidValue(ValueQualityStatistics valueQuality, String invalidValue) {
-        if (isStoreInvalidValues) {
-            valueQuality.appendInvalidValue(invalidValue);
-        }
+        return status;
     }
 
     public void end() {
@@ -93,30 +91,30 @@ public class ValueQualityAnalyzer implements Analyzer<ValueQualityStatistics> {
     }
 
     public List<ValueQualityStatistics> getResult() {
-        return results;
+        if (semanticQualityAnalyzer == null) {
+            return dataTypeQualityAnalyzer.getResult();
+        } else {
+            List<ValueQualityStatistics> aggregatedResult = new ArrayList<ValueQualityStatistics>();
+            List<ValueQualityStatistics> dataTypeQualityResult = dataTypeQualityAnalyzer.getResult();
+            List<ValueQualityStatistics> semanticQualityResult = semanticQualityAnalyzer.getResult();
+
+            for (int i = 0; i < dataTypeQualityResult.size(); i++) {
+                if ("UNKNOWN".equals(semanticQualityAnalyzer.getTypes()[i])) {
+                    aggregatedResult.add(dataTypeQualityResult.get(i));
+                } else {
+                    aggregatedResult.add(semanticQualityResult.get(i));
+                }
+            }
+            return aggregatedResult;
+        }
     }
 
     public Analyzer<ValueQualityStatistics> merge(Analyzer<ValueQualityStatistics> analyzer) {
-        int idx = 0;
-        ValueQualityAnalyzer mergedValueQualityAnalyze = new ValueQualityAnalyzer();
-        ((ResizableList<ValueQualityStatistics>) mergedValueQualityAnalyze.getResult()).resize(results.size());
-        for (ValueQualityStatistics qs : results) {
-            ValueQualityStatistics mergedStats = mergedValueQualityAnalyze.getResult().get(idx);
-            ValueQualityStatistics anotherStats = analyzer.getResult().get(idx);
-            mergedStats.setValidCount(qs.getValidCount() + anotherStats.getValidCount());
-            mergedStats.setInvalidCount(qs.getInvalidCount() + anotherStats.getInvalidCount());
-            mergedStats.setEmptyCount(qs.getEmptyCount() + anotherStats.getEmptyCount());
-            if (!qs.getInvalidValues().isEmpty()) {
-                mergedStats.getInvalidValues().addAll(qs.getInvalidValues());
-            }
-            if (!anotherStats.getInvalidValues().isEmpty()) {
-                mergedStats.getInvalidValues().addAll(anotherStats.getInvalidValues());
-            }
-            idx++;
-        }
-        return mergedValueQualityAnalyze;
+        throw new NotImplementedException();
     }
+
     @Override
     public void close() throws Exception {
     }
+
 }
--- main/plugins/org.talend.dataquality.statistics/src/test/java/org/talend/dataquality/statistics/quality/ValueQualityAnalyzerTest.java
+++ main/plugins/org.talend.dataquality.statistics/src/test/java/org/talend/dataquality/statistics/quality/ValueQualityAnalyzerTest.java
@@ -13,20 +13,18 @@
 package org.talend.dataquality.statistics.quality;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import org.talend.datascience.common.inference.Analyzer;
+import org.talend.dataquality.semantic.classifier.SemanticCategoryEnum;
+import org.talend.dataquality.semantic.statistics.SemanticQualityAnalyzer;
+import org.talend.datascience.common.inference.ValueQualityStatistics;
 import org.talend.datascience.common.inference.type.DataType;
 import org.talend.datascience.common.inference.type.DataType.Type;
 
@@ -35,314 +33,6 @@ import org.talend.datascience.common.inference.type.DataType.Type;
  *
  */
 public class ValueQualityAnalyzerTest {
-
-    private ValueQualityAnalyzer analyzer;
-
-    private static void populateAnalyzeData(ValueQualityAnalyzer qualityAnalyzer) {
-        qualityAnalyzer.analyze("1", "");
-        qualityAnalyzer.analyze("2", "a");
-        qualityAnalyzer.analyze("3", "2.0");
-        qualityAnalyzer.analyze("str", "0.1");
-        qualityAnalyzer.analyze("another str", "");
-    }
-
-    private static void populateAnalyzerNoneString(ValueQualityAnalyzer qualityAnalyzer) {
-        qualityAnalyzer.analyze("1.0");
-        qualityAnalyzer.analyze("0.02");
-        qualityAnalyzer.analyze("2.88888888888888888888888");
-        qualityAnalyzer.analyze("3");
-        qualityAnalyzer.analyze("5538297118");
-        qualityAnalyzer.analyze("str");
-    }
-    private static void populateAnalyzerHalfA(ValueQualityAnalyzer qualityAnalyzer) {
-        qualityAnalyzer.analyze("1.0");
-        qualityAnalyzer.analyze("0.02");
-        qualityAnalyzer.analyze("2.88888888888888888888888");
-    }
-    private static void populateAnalyzerHalfB(ValueQualityAnalyzer qualityAnalyzer) {
-        qualityAnalyzer.analyze("3");
-        qualityAnalyzer.analyze("5538297118");
-        qualityAnalyzer.analyze("str");
-    }
-
-    private static void populateAnalyzerWithNumbers(ValueQualityAnalyzer qualityAnalyzer) {
-        qualityAnalyzer.analyze("1.0");
-        qualityAnalyzer.analyze("0.02");
-        qualityAnalyzer.analyze("2.88888888888888888888888");
-        qualityAnalyzer.analyze("3.0");
-        qualityAnalyzer.analyze("5538297118");
-    }
-
-    @Before
-    public void setUp() {
-        analyzer = new ValueQualityAnalyzer(DataType.Type.INTEGER);
-        analyzer.init();
-    }
-
-    @After
-    public void tearDown() {
-        analyzer.end();
-    }
-
-    @Test
-    public void testEmpty() throws Exception {
-        // One empty record
-        analyzer.analyze("");
-        assertEquals(1, analyzer.getResult().size());
-        assertEquals(1, analyzer.getResult().get(0).getEmptyCount());
-        assertEquals(0, analyzer.getResult().get(0).getInvalidCount());
-        assertEquals(0, analyzer.getResult().get(0).getValidCount());
-        // One new empty record
-        analyzer.analyze("");
-        assertEquals(2, analyzer.getResult().get(0).getEmptyCount());
-        assertEquals(0, analyzer.getResult().get(0).getInvalidCount());
-        assertEquals(0, analyzer.getResult().get(0).getValidCount());
-    }
-
-    @Test
-    public void testValid() throws Exception {
-        // One valid record (type is integer)
-        analyzer.analyze("1");
-        assertEquals(1, analyzer.getResult().size());
-        assertEquals(0, analyzer.getResult().get(0).getEmptyCount());
-        assertEquals(0, analyzer.getResult().get(0).getInvalidCount());
-        assertEquals(1, analyzer.getResult().get(0).getValidCount());
-        // One new valid record (type is still integer)
-        analyzer.analyze("2");
-        assertEquals(0, analyzer.getResult().get(0).getEmptyCount());
-        assertEquals(0, analyzer.getResult().get(0).getInvalidCount());
-        assertEquals(2, analyzer.getResult().get(0).getValidCount());
-    }
-
-    @Test
-    public void testInvalid() throws Exception {
-        // One invalid record (type is integer)
-        analyzer.analyze("aaaa");
-        assertEquals(1, analyzer.getResult().size());
-        assertEquals(0, analyzer.getResult().get(0).getEmptyCount());
-        assertEquals(1, analyzer.getResult().get(0).getInvalidCount());
-        assertEquals(0, analyzer.getResult().get(0).getValidCount());
-        // One new invalid record (type is integer)
-        analyzer.analyze("bbbb");
-        assertEquals(0, analyzer.getResult().get(0).getEmptyCount());
-        assertEquals(2, analyzer.getResult().get(0).getInvalidCount());
-        assertEquals(0, analyzer.getResult().get(0).getValidCount());
-    }
-
-    @Test
-    public void testInvalidValues() {
-        ValueQualityAnalyzer qualityAnalyzer = new ValueQualityAnalyzer(DataType.Type.STRING);
-        qualityAnalyzer.analyze("1");
-        qualityAnalyzer.analyze("2");
-        qualityAnalyzer.analyze("3");
-        qualityAnalyzer.analyze("str");
-        qualityAnalyzer.analyze("another str");
-        // Valid and invalid
-        assertEquals(0, qualityAnalyzer.getResult().get(0).getInvalidCount());
-        assertEquals(5, qualityAnalyzer.getResult().get(0).getValidCount());
-        // Invalid values
-        Set<String> invalidValues = qualityAnalyzer.getResult().get(0).getInvalidValues();
-        assertEquals(0, invalidValues.size());
-    }
-
-    @Test
-    public void testTwoColumnsInvalid() {
-        ValueQualityAnalyzer qualityAnalyzer = new ValueQualityAnalyzer(DataType.Type.INTEGER, DataType.Type.DOUBLE);
-        populateAnalyzeData(qualityAnalyzer);
-        // --- Assert column 0
-        ValueQualityStatistics valueQuality = qualityAnalyzer.getResult().get(0);
-        // Valid and invalid
-        assertEquals(2, valueQuality.getInvalidCount());
-        assertEquals(3, valueQuality.getValidCount());
-        // Invalid values
-        Set<String> invalidValues = valueQuality.getInvalidValues();
-        assertTrue(invalidValues.contains("str"));
-        assertTrue(invalidValues.contains("another str"));
-        // ---Assert column 1
-        valueQuality = qualityAnalyzer.getResult().get(1);
-        // Valid , Empty, and invalid
-        assertEquals(1, valueQuality.getInvalidCount());
-        assertEquals(2, valueQuality.getValidCount());
-        assertEquals(2, valueQuality.getEmptyCount());
-        // Invalid values
-        invalidValues = valueQuality.getInvalidValues();
-        assertEquals(1, invalidValues.size());
-        assertTrue(invalidValues.contains("a"));
-
-    }
-
-    @Test
-    public void testTwoColumnsValidAndEmpty() throws Exception {
-        // ---Assert when user set string type
-        ValueQualityAnalyzer qualityAnalyzer = new ValueQualityAnalyzer(DataType.Type.STRING, DataType.Type.STRING);
-        populateAnalyzeData(qualityAnalyzer);
-        ValueQualityStatistics valueQuality = qualityAnalyzer.getResult().get(0);
-        // Valid and invalid
-        assertEquals(0, valueQuality.getInvalidCount());
-        assertEquals(5, valueQuality.getValidCount());
-        // Invalid values
-        Set<String> invalidValues = valueQuality.getInvalidValues();
-        assertEquals(0, invalidValues.size());
-
-        // --- test when user set actual type to string
-        valueQuality = qualityAnalyzer.getResult().get(1);
-        // Valid , Empty, and invalid
-        assertEquals(0, valueQuality.getInvalidCount());
-        assertEquals(3, valueQuality.getValidCount());
-        assertEquals(2, valueQuality.getEmptyCount());
-        // Invalid values
-        invalidValues = valueQuality.getInvalidValues();
-        assertEquals(0, invalidValues.size());
-    }
-
-    @Test
-    public void testValidIntegers() {
-        ValueQualityAnalyzer qualityAnalyzer = new ValueQualityAnalyzer(DataType.Type.INTEGER);
-        qualityAnalyzer.analyze("1");
-        qualityAnalyzer.analyze("2");
-        qualityAnalyzer.analyze("3");
-        qualityAnalyzer.analyze("5538297118");
-
-        // Valid and invalid
-        ValueQualityStatistics valueQuality = qualityAnalyzer.getResult().get(0);
-        assertEquals(0, valueQuality.getInvalidCount());
-        assertEquals(4, valueQuality.getValidCount());
-        // Invalid values
-        Set<String> invalidValues = valueQuality.getInvalidValues();
-        assertEquals(0, invalidValues.size());
-    }
-
-    @Test
-    public void testNoneStrings_double() {
-        ValueQualityAnalyzer qualityAnalyzer = new ValueQualityAnalyzer(DataType.Type.DOUBLE);
-        populateAnalyzerNoneString(qualityAnalyzer);
-        ValueQualityStatistics valueQuality = qualityAnalyzer.getResult().get(0);
-        // Valid and invalid
-        assertEquals(1, valueQuality.getInvalidCount());
-        assertEquals(5, valueQuality.getValidCount());
-        // Invalid values
-        Set<String> invalidValues = valueQuality.getInvalidValues();
-        assertEquals(1, invalidValues.size());
-        assertTrue(invalidValues.contains("str"));
-    }
-
-    @Test
-    public void testNoneStrings_string() throws Exception {
-        // ---- second pass when user set the user defined type as string---
-        // Actual type
-        ValueQualityAnalyzer qualityAnalyzer = new ValueQualityAnalyzer(DataType.Type.STRING);
-        populateAnalyzerNoneString(qualityAnalyzer);
-        ValueQualityStatistics valueQuality = qualityAnalyzer.getResult().get(0);
-        // Valid and invalid
-        assertEquals(0, valueQuality.getInvalidCount());
-        assertEquals(6, valueQuality.getValidCount());
-        // Invalid values
-        Set<String> invalidValues = valueQuality.getInvalidValues();
-        assertEquals(0, invalidValues.size());
-    }
-
-    @Test
-    public void testNoneStrings_integer() {
-        // ---- third pass when user set the user defined type as Integer---
-        // Actual type
-        ValueQualityAnalyzer qualityAnalyzer = new ValueQualityAnalyzer(DataType.Type.INTEGER);
-        populateAnalyzerNoneString(qualityAnalyzer);
-        ValueQualityStatistics valueQuality = qualityAnalyzer.getResult().get(0);
-        // Valid and invalid
-        assertEquals(4, valueQuality.getInvalidCount());
-        assertEquals(2, valueQuality.getValidCount());
-        // Invalid values
-        Set<String> invalidValues = valueQuality.getInvalidValues();
-        assertEquals(4, invalidValues.size());
-        assertTrue(invalidValues.contains("1.0"));
-        assertTrue(invalidValues.contains("0.02"));
-        assertTrue(invalidValues.contains("2.88888888888888888888888"));
-        assertTrue(invalidValues.contains("str"));
-    }
-
-    @Test
-    public void testNumbers_double() {
-        ValueQualityAnalyzer qualityAnalyzer = new ValueQualityAnalyzer(DataType.Type.DOUBLE);
-        populateAnalyzerWithNumbers(qualityAnalyzer);
-        ValueQualityStatistics valueQuality = qualityAnalyzer.getResult().get(0);
-        // Valid and invalid
-        assertEquals(0, valueQuality.getInvalidCount());
-        assertEquals(5, valueQuality.getValidCount());
-        // Invalid values
-        Set<String> invalidValues = valueQuality.getInvalidValues();
-        assertEquals(0, invalidValues.size());
-    }
-
-    @Test
-    public void testNumbers_string() {
-        // ---- send pass when user set the user defined type ---
-        // Actual type
-        ValueQualityAnalyzer qualityAnalyzer = new ValueQualityAnalyzer(DataType.Type.STRING);
-        populateAnalyzerWithNumbers(qualityAnalyzer);
-        ValueQualityStatistics valueQuality = qualityAnalyzer.getResult().get(0);
-        // Valid and invalid
-        assertEquals(0, valueQuality.getInvalidCount());
-        assertEquals(5, valueQuality.getValidCount());
-        // Invalid values
-        Set<String> invalidValues = valueQuality.getInvalidValues();
-        assertEquals(0, invalidValues.size());
-    }
-
-    @Test
-    public void testEmptyOver() {
-        ValueQualityAnalyzer qualityAnalyzer = new ValueQualityAnalyzer(DataType.Type.STRING);
-        qualityAnalyzer.analyze("");
-        qualityAnalyzer.analyze("");
-        qualityAnalyzer.analyze("");
-        qualityAnalyzer.analyze("");
-        qualityAnalyzer.analyze("1");
-        qualityAnalyzer.analyze("a str");
-        ValueQualityStatistics valueQuality = qualityAnalyzer.getResult().get(0);
-        // Valid and invalid
-        assertEquals(0, valueQuality.getInvalidCount());
-        assertEquals(2, valueQuality.getValidCount());
-        assertEquals(4, valueQuality.getEmptyCount());
-        // Invalid values
-        Set<String> invalidValues = valueQuality.getInvalidValues();
-        assertEquals(0, invalidValues.size());
-    }
-
-    @Test
-    public void testEmptyAll() {
-        ValueQualityAnalyzer qualityAnalyzer = new ValueQualityAnalyzer(DataType.Type.STRING);
-        qualityAnalyzer.analyze("");
-        qualityAnalyzer.analyze("");
-        qualityAnalyzer.analyze(null);
-        qualityAnalyzer.analyze("");
-        qualityAnalyzer.analyze(" ");
-        qualityAnalyzer.analyze("  ");
-        ValueQualityStatistics valueQuality = qualityAnalyzer.getResult().get(0);
-        // Valid and invalid
-        assertEquals(0, valueQuality.getInvalidCount());
-        assertEquals(0, valueQuality.getValidCount());
-        assertEquals(6, valueQuality.getEmptyCount());
-        // Invalid values
-        Set<String> invalidValues = valueQuality.getInvalidValues();
-        assertEquals(0, invalidValues.size());
-    }
-
-    @Test
-    public void testInvalidCount() {// for issue TDQ_10380
-        ValueQualityAnalyzer qualityAnalyzer = new ValueQualityAnalyzer(new Type[] { DataType.Type.INTEGER, Type.STRING,
-                Type.STRING, Type.STRING, Type.DATE, Type.STRING, Type.DATE, Type.INTEGER, Type.DOUBLE });
-
-        final List<String[]> records = getRecords(this.getClass().getResourceAsStream("../data/customers_100.csv"));
-
-        for (String[] record : records) {
-            qualityAnalyzer.analyze(record);
-        }
-
-        ValueQualityStatistics result = qualityAnalyzer.getResult().get(7);
-        assertEquals(6, result.getInvalidCount());
-        assertEquals(4, result.getEmptyCount());
-        assertEquals(90, result.getValidCount());
-        assertEquals(100, result.getCount());
-    }
 
     public static List<String[]> getRecords(InputStream inputStream, String separator) {
         if (inputStream == null) {
@@ -369,29 +59,87 @@ public class ValueQualityAnalyzerTest {
 
     }
 
-    @Test
-    public void testMerge() {
-        ValueQualityAnalyzer qualityAnalyzer = new ValueQualityAnalyzer(DataType.Type.DOUBLE);
-        ValueQualityAnalyzer qualityAnalyzer2 = new ValueQualityAnalyzer(DataType.Type.DOUBLE);
-
-        
-        populateAnalyzerHalfA(qualityAnalyzer);
-        populateAnalyzerHalfB(qualityAnalyzer2);
-       
-        Analyzer<ValueQualityStatistics> mergedAnalyzer =   qualityAnalyzer.merge(qualityAnalyzer2);
-        ValueQualityStatistics valueQuality = mergedAnalyzer.getResult().get(0);
-        // Valid and invalid
-        assertEquals(1, valueQuality.getInvalidCount());
-        assertEquals(5, valueQuality.getValidCount());
-        // Invalid values
-        Set<String> invalidValues = valueQuality.getInvalidValues();
-        assertEquals(1, invalidValues.size());
-        assertTrue(invalidValues.contains("str"));
-        
-        
-    }
-
     public static List<String[]> getRecords(InputStream inputStream) {
         return getRecords(inputStream, ";");
+    }
+
+    @Test
+    public void testValueQualityAnalyzerWithoutSemanticQuality() {
+
+        DataTypeQualityAnalyzer dataTypeQualityAnalyzer = new DataTypeQualityAnalyzer(new Type[] { DataType.Type.INTEGER,
+                Type.STRING, Type.STRING, Type.STRING, Type.DATE, Type.STRING, Type.DATE, Type.INTEGER, Type.DOUBLE });
+        String[] semanticTypes = new String[] { SemanticCategoryEnum.UNKNOWN.name(), SemanticCategoryEnum.UNKNOWN.name(),
+                SemanticCategoryEnum.UNKNOWN.name(), SemanticCategoryEnum.UNKNOWN.name(), SemanticCategoryEnum.UNKNOWN.name(),
+                SemanticCategoryEnum.UNKNOWN.name(), SemanticCategoryEnum.UNKNOWN.name(), SemanticCategoryEnum.UNKNOWN.name(),
+                SemanticCategoryEnum.UNKNOWN.name() };
+        SemanticQualityAnalyzer semanticQualityAnalyzer = new SemanticQualityAnalyzer(semanticTypes);
+
+        ValueQualityAnalyzer valueQualityAnalyzer = new ValueQualityAnalyzer(dataTypeQualityAnalyzer, semanticQualityAnalyzer);
+        valueQualityAnalyzer.init();
+
+        final List<String[]> records = getRecords(this.getClass().getResourceAsStream("../data/customers_100.csv"));
+
+        for (String[] record : records) {
+            valueQualityAnalyzer.analyze(record);
+        }
+
+        for (int i = 0; i < semanticTypes.length; i++) {
+            ValueQualityStatistics dataTypeQualityResult = dataTypeQualityAnalyzer.getResult().get(i);
+            ValueQualityStatistics aggregatedResult = valueQualityAnalyzer.getResult().get(i);
+            assertEquals("unexpected ValidCount on Column " + i, dataTypeQualityResult.getValidCount(),
+                    aggregatedResult.getValidCount());
+            assertEquals("unexpected InvalidCount on Column " + i, dataTypeQualityResult.getInvalidCount(),
+                    aggregatedResult.getInvalidCount());
+            assertEquals("unexpected EmptyCount on Column " + i, dataTypeQualityResult.getEmptyCount(),
+                    aggregatedResult.getEmptyCount());
+        }
+
+        try {
+            valueQualityAnalyzer.close();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testValueQualityAnalyzerWithSemanticQuality() {
+
+        final List<String[]> records = new ArrayList<String[]>() {
+
+            private static final long serialVersionUID = 1L;
+
+            {
+                add(new String[] { "1", "UT" });
+                add(new String[] { "2", "MN" });
+                add(new String[] { "3", "MO" });
+                add(new String[] { "4", "" });
+                add(new String[] { "5", "IL" });
+                add(new String[] { "6", "ORZ" });
+                add(new String[] { "7", " " });
+            }
+        };
+
+        DataTypeQualityAnalyzer dataTypeQualityAnalyzer = new DataTypeQualityAnalyzer(new Type[] { DataType.Type.INTEGER,
+                Type.STRING });
+        SemanticQualityAnalyzer semanticQualityAnalyzer = new SemanticQualityAnalyzer(new String[] {
+                SemanticCategoryEnum.UNKNOWN.name(), SemanticCategoryEnum.US_STATE_CODE.name() });
+
+        ValueQualityAnalyzer qualityAnalyzer = new ValueQualityAnalyzer(dataTypeQualityAnalyzer, semanticQualityAnalyzer);
+        qualityAnalyzer.init();
+
+        for (String[] record : records) {
+            qualityAnalyzer.analyze(record);
+        }
+
+        ValueQualityStatistics aggregatedResult = qualityAnalyzer.getResult().get(1);
+
+        System.out.println(aggregatedResult);
+        try {
+            qualityAnalyzer.close();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
