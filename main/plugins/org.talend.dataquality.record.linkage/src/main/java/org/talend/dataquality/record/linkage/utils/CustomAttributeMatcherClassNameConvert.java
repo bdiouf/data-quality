9cce09ab4bf578e7eb5567294431a233799e5333
zshen-talend <zshen@talend.com>
2015-11-27 18:06:39 +0800

    bug fixed TDQ-11264 add commont on the helper method

--- main/plugins/org.talend.dataquality.record.linkage/src/main/java/org/talend/dataquality/record/linkage/utils/CustomAttributeMatcherClassNameConvert.java
+++ main/plugins/org.talend.dataquality.record.linkage/src/main/java/org/talend/dataquality/record/linkage/utils/CustomAttributeMatcherClassNameConvert.java
@@ -34,9 +34,10 @@ public class CustomAttributeMatcherClassNameConvert {
 
     /**
      * DOC zshen Comment method "getClassName".
-     *
-     * @param classPathParameter
-     * @return
+     * 
+     * @param classPathParameter like "CustomMatcherTest.jar||MycustomMatch.jar||testCustomMatcher.myCustomMatcher" last
+     * one is fully qualified name("testCustomMatcher" is package name and "myCustomMatcher" is class name).
+     * @return the class name as the demo will return testCustomMatcher.myCustomMatcher
      */
     public static String getClassName(String classPathParameter) {
         String[] allElements = classPathParameter.split(REGEXKEY);
