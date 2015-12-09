ca036d5cb13c05a5fb0cc2718952541b8edc669e
fangrou <fji@talend.com>
2015-12-09 12:14:47 +0100

    TDQ-11229 Cleanse the class: DateTimePatternListGenerator

--- main/plugins/org.talend.dataquality.statistics/src/test/java/org/talend/dataquality/statistics/type/DateTimePatternListGenerator.java
+++ main/plugins/org.talend.dataquality.statistics/src/test/java/org/talend/dataquality/statistics/type/DateTimePatternListGenerator.java
@@ -23,7 +23,7 @@ public class DateTimePatternListGenerator {
     private final static FormatStyle[] FORMAT_STYLES = new FormatStyle[] { FormatStyle.SHORT, FormatStyle.MEDIUM,
             FormatStyle.LONG, FormatStyle.FULL };
 
-    private static Set<String> getBaseDateTimePatternsByLocales() {
+    private static List<String> getBaseDateTimePatternsByLocales(List<String> knownPatternList) {
 
         Locale[] localeArray = new Locale[] { Locale.US, //
                 Locale.FRANCE, //
@@ -39,75 +39,104 @@ public class DateTimePatternListGenerator {
         for (Locale l : localeArray) {
             getFormatsOfLocale(dateTimePatternsSet, l, true);
         }
-        return dateTimePatternsSet;
+        dateTimePatternsSet.removeAll(knownPatternList);
+        return new ArrayList<String>(dateTimePatternsSet);
 
     }
 
-    private static Set<String> getAdditionalDateTimePatternsByLocales() {
+    private static List<String> getAdditionalDateTimePatternsByLocales(List<String> knownPatternList) {
 
         Set<String> dateTimePatternsSet = new LinkedHashSet<String>();
 
         for (String lang : Locale.getISOLanguages()) {
             getFormatsOfLocale(dateTimePatternsSet, new Locale(lang), false);
         }
+        dateTimePatternsSet.removeAll(knownPatternList);
 
-        return dateTimePatternsSet;
+        return new ArrayList<String>(dateTimePatternsSet);
 
     }
 
-    private static void getFormatsOfLocale(Set<String> dateTimePatternsSet, Locale locale, boolean keepLongMonth) {
-        System.out.println("------------------------------------------------------");
-        System.out.println("Locale: " + locale);
-        getFormatByStyle(true, false, dateTimePatternsSet, locale, keepLongMonth);
-        getFormatByStyle(true, true, dateTimePatternsSet, locale, keepLongMonth);
+    private static List<String> getISOAndRFCDateTimePatternList(List<String> knownPatternList) {
+
+        List<String> patternList = new ArrayList<String>();
+
+        patternList.add("yyyyMMddZ");// 1. BASIC_ISO_DATE
+        patternList.add("yyyy-MM-ddZZZZZ");// 2. ISO_DATE
+        patternList.add("yyyy-MM-dd'T'HH:mm:ss.SSS");// 3. ISO_DATE_TIME
+        patternList.add("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");// 4. ISO_INSTANT
+        patternList.add("yyyy-MM-dd");// 5. ISO_LOCAL_DATE
+        patternList.add("yyyy-MM-dd'T'HH:mm:ss.SSS");// 6. ISO_LOCAL_DATE_TIME
+        patternList.add("yyyy-MM-ddZZZZZ");// 7. ISO_OFFSET_DATE
+        patternList.add("yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ");// 8. ISO_OFFSET_DATE_TIME
+        patternList.add("yyyy-DZZZZZ");// 9. ISO_ORDINAL_DATE
+        patternList.add("yyyy-'W'w-WZZZZZ");// 10. ISO_WEEK_DATE
+        patternList.add("yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ'['VV']'");// 11. ISO_ZONED_DATE_TIME
+        patternList.add("EEE, d MMM yyyy HH:mm:ss Z");// 12. RFC_1123_DATE_TIME
+
+        patternList.removeAll(knownPatternList);
+
+        return patternList;
     }
 
-    private static void getFormatByStyle(boolean isDateRequired, boolean isTimeRequired, Set<String> dateTimePatternsSet,
-            Locale locale, boolean keepLongMonth) {
-        for (FormatStyle style : FORMAT_STYLES) {
-            String pattern = DateTimeFormatterBuilder.getLocalizedDateTimePattern(//
-                    isDateRequired ? style : null, isTimeRequired ? style : null, IsoChronology.INSTANCE, locale);//
 
-            // pattern = pattern.replace("dd", "d").replace("MMMM", "4444").replace("MMM", "333").replace("MM", "M")
-            // .replace("333", "MMM").replace("4444", "MMMM").replace("HH", "H").replace("hh", "h").replace("mm", "m")
-            // .replace("ss", "s");
+    private static List<String> getNonExistentPatternsInOldFile(List<String> knownPatternList) {
+        List<String> list = new ArrayList<String>(SystemDatetimePatternManager.DATE_PATTERN_NAMES);
 
-            if (!dateTimePatternsSet.contains(pattern)) {
-                if (!keepLongMonth && (pattern.contains("MMMM") || pattern.contains("MMM") || pattern.contains("a"))) {
-                    continue;
-                }
-                DateTimeFormatter dtf = DateTimeFormatter.ofPattern(pattern, locale);
+        List<String> nonExistentPatterns = new ArrayList<String>();
 
-                System.out.println(pattern + " \t" + ZONED_DATE_TIME.format(dtf));
-                dateTimePatternsSet.add(pattern);
-            }
-        }
-    }
-
-    private static List<String> calculateISOList() {
-        List<String> list = new ArrayList<String>(getBaseDateTimePatternsByLocales());
-        sortDatePattern(list);
-
-        System.out.println("---------------------Base Patterns----------------------");
+        // sortDatePattern(list);
         for (String pattern : list) {
-            System.out.println(pattern);
-        }
-
-        List<String> list2 = new ArrayList<String>(getAdditionalDateTimePatternsByLocales());
-        sortDatePattern(list2);
-
-        System.out.println("---------------------Additional Patterns----------------------");
-        for (String pattern : list2) {
-            if (!list.contains(pattern)) {
-                System.out.println(pattern);
+            if (!knownPatternList.contains(pattern)) {
+                nonExistentPatterns.add(pattern);// TODO: add the pattern according to the regex not "pattern text"
+                System.out.println(pattern + " \t" + getFormattedDateTime(pattern, Locale.US));
             }
         }
-
-        list.addAll(list2);
-        return list;
+        return nonExistentPatterns;
 
     }
 
+    @SuppressWarnings("unused")
+    private static void validateISOPattens(List<String> isoPatternList) {
+
+        Set<String> formattedDateTimeSet = new HashSet<String>();
+        for (String pattern : isoPatternList) {
+            formattedDateTimeSet.add(getFormattedDateTime(pattern, Locale.US));
+        }
+
+        DateTimeFormatter[] formatters = new DateTimeFormatter[] { DateTimeFormatter.BASIC_ISO_DATE, // 1
+                DateTimeFormatter.ISO_DATE, // 2
+                DateTimeFormatter.ISO_DATE_TIME, // 3
+                // DateTimeFormatter.ISO_TIME, //
+                DateTimeFormatter.ISO_INSTANT, // 4
+                DateTimeFormatter.ISO_LOCAL_DATE, // 5
+                DateTimeFormatter.ISO_LOCAL_DATE_TIME,// 6
+                // DateTimeFormatter.ISO_LOCAL_TIME, //
+                DateTimeFormatter.ISO_OFFSET_DATE, // 7
+                DateTimeFormatter.ISO_OFFSET_DATE_TIME, // 8
+                // DateTimeFormatter.ISO_OFFSET_TIME, //
+                DateTimeFormatter.ISO_ORDINAL_DATE, // 9
+                DateTimeFormatter.ISO_WEEK_DATE, // 10
+                DateTimeFormatter.ISO_ZONED_DATE_TIME, // 11
+                DateTimeFormatter.RFC_1123_DATE_TIME, // 12
+        };
+
+        System.out.println("-------------Validate ISO PattenText-------------");
+        for (int i = 0; i < formatters.length; i++) {
+
+            System.out.print((i + 1) + "\t");
+            try {
+                String formattedDateTime = ZONED_DATE_TIME.format(formatters[i]);
+                System.out.print(formattedDateTimeSet.contains(formattedDateTime) ? "YES\t" : "NO\t");
+                System.out.println(formattedDateTime);
+            } catch (Throwable t) {
+                System.out.println(t.getMessage());
+            }
+        }
+
+    }
+
+    // TODO Sort not only by length but also by other conditions: DecimalStyle, hasZone? etc.
     private static void sortDatePattern(List<String> list) {
         Collections.sort(list, new Comparator<String>() {
 
@@ -128,59 +157,30 @@ public class DateTimePatternListGenerator {
         });
     }
 
-    private static List<String> getNonExistentPatternsInOldFile(Set<String> old, List<String> lookup) {
-        List<String> list = new ArrayList<String>(old);
-
-        List<String> nonExistentPatterns = new ArrayList<String>();
-
-        sortDatePattern(list);
-        for (String pattern : list) {
-            if (!lookup.contains(pattern)) {
-                nonExistentPatterns.add(pattern);
-                System.out.println("OLD >> " + pattern + " \t" + getFormattedDateTime(pattern));
-            }
-        }
-        return nonExistentPatterns;
+    private static void getFormatsOfLocale(Set<String> dateTimePatternsSet, Locale locale, boolean keepLongMonth) {
+        getFormatByStyle(true, false, dateTimePatternsSet, locale, keepLongMonth);
+        getFormatByStyle(true, true, dateTimePatternsSet, locale, keepLongMonth);
 
     }
 
-    private static List<String> getOtherISOFormats(Set<String> formattedDateTimeSet) {
+    private static void getFormatByStyle(boolean isDateRequired, boolean isTimeRequired, Set<String> dateTimePatternsSet,
+            Locale locale, boolean keepLongMonth) {
+        for (FormatStyle style : FORMAT_STYLES) {
+            String pattern = DateTimeFormatterBuilder.getLocalizedDateTimePattern(//
+                    isDateRequired ? style : null, isTimeRequired ? style : null, IsoChronology.INSTANCE, locale);//
 
-        DateTimeFormatter[] formatters = new DateTimeFormatter[] { DateTimeFormatter.BASIC_ISO_DATE, // 1
-                DateTimeFormatter.ISO_DATE, // 2
-                DateTimeFormatter.ISO_DATE_TIME, // 3
-                // DateTimeFormatter.ISO_TIME, //
-                DateTimeFormatter.ISO_INSTANT, // 4
-                DateTimeFormatter.ISO_LOCAL_DATE, // 5
-                DateTimeFormatter.ISO_LOCAL_DATE_TIME,// 6
-                // DateTimeFormatter.ISO_LOCAL_TIME, //
-                DateTimeFormatter.ISO_OFFSET_DATE, // 7
-                DateTimeFormatter.ISO_OFFSET_DATE_TIME, // 8
-                // DateTimeFormatter.ISO_OFFSET_TIME, //
-                DateTimeFormatter.ISO_ORDINAL_DATE, // 9
-                DateTimeFormatter.ISO_WEEK_DATE, // 10
-                DateTimeFormatter.ISO_ZONED_DATE_TIME, // 11
-                DateTimeFormatter.RFC_1123_DATE_TIME, // 12
-        };
+            if (!dateTimePatternsSet.contains(pattern)) {
+                if (!keepLongMonth && (pattern.contains("MMMM") || pattern.contains("MMM") || pattern.contains("a"))) {
+                    continue;
+                }
 
-        System.out.println("-------------Other ISO formats-------------");
-        for (int i = 0; i < formatters.length; i++) {
-
-            System.out.print((i + 1) + "\t");
-            try {
-                String formattedDateTime = ZONED_DATE_TIME.format(formatters[i]);
-                System.out.print(formattedDateTimeSet.contains(formattedDateTime) ? "YES\t" : "NO\t");
-                System.out.println(formattedDateTime);
-            } catch (Throwable t) {
-                System.out.println(t.getMessage());
+                dateTimePatternsSet.add(pattern);
             }
         }
-
-        return new ArrayList<String>();
     }
 
-    private static String getFormattedDateTime(String pattern) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+    private static String getFormattedDateTime(String pattern, Locale locale) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern, locale);
         try {
             String formattedDateTime = ZONED_DATE_TIME.format(formatter);
             return formattedDateTime;
@@ -190,37 +190,42 @@ public class DateTimePatternListGenerator {
     }
 
     public static void main(String[] args) {
-        Locale.setDefault(Locale.US);
 
-        List<String> patternList = calculateISOList();
-        System.out.println("ISO count: " + patternList.size());
+        List<String> knownPatternList = new ArrayList<String>();
 
-        List<String> oldPatterns = getNonExistentPatternsInOldFile(SystemDatetimePatternManager.DATE_PATTERN_NAMES, patternList);
-        System.out.println("OLD count: " + oldPatterns.size());
-        patternList.addAll(oldPatterns);
-
-        System.out.println("\nTotal count: " + patternList.size());
-
-        patternList.add("yyyyMMddZ");// 1
-        patternList.add("yyyy-MM-ddZZZZZ");// 2
-        patternList.add("yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ'['VV']'");// 3
-        patternList.add("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");// 4
-
-        patternList.add("yyyy-MM-dd'T'HH:mm:ss.SSS");// 6
-
-        patternList.add("yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ");// 8
-        patternList.add("yyyy-DZZZZZ");// 9
-        patternList.add("yyyy-'W'w-WZZZZZ");// 10
-        patternList.add("yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ'['VV']'");// 11
-        patternList.add("EEE, d MMM yyyy HH:mm:ss Z");// 12
-
-        Set<String> formattedDateTimeSet = new HashSet<String>();
-        for (String pattern : patternList) {
-            formattedDateTimeSet.add(getFormattedDateTime(pattern));
+        // 1. Base Localized DateTimePatterns (java8 DateTimeFormatterBuilder)
+        List<String> basePatternList = getBaseDateTimePatternsByLocales(knownPatternList);
+        for (String pattern : basePatternList) {
             System.out.println(pattern);
         }
+        knownPatternList.addAll(basePatternList);
+        System.out.println("#basePatterns = " + basePatternList.size() + "\n");
 
-        List<String> isoPatterns = getOtherISOFormats(formattedDateTimeSet);
+        // 2. Additional Localized DateTimePatterns (java8 DateTimeFormatterBuilder)
+        List<String> additionalPatternList = getAdditionalDateTimePatternsByLocales(knownPatternList);
+        for (String pattern : additionalPatternList) {
+            System.out.println(pattern);
+        }
+        knownPatternList.addAll(additionalPatternList);
+        System.out.println("#additionalPatternList = " + additionalPatternList.size() + "\n");
+
+        // 3. ISO and RFC DateTimePatterns
+        List<String> isoPatternList = getISOAndRFCDateTimePatternList(knownPatternList);
+        // validateISOPattens(isoPatternList);
+        for (String pattern : isoPatternList) {
+            System.out.println(pattern);
+        }
+        knownPatternList.addAll(isoPatternList);
+        System.out.println("#DateTimePattern(ISO&RFC) = " + isoPatternList.size() + "\n");
+
+        // TODO 4. add old DateTimePatterns
+        // getNonExistentPatternsInOldFile(knownPatternList);
+
+        System.out.println("#Total = " + knownPatternList.size() + //
+                " (#basePatterns = " + basePatternList.size() + //
+                ", #additionalPatterns = " + additionalPatternList.size() + //
+                ", #isoPatterns = " + isoPatternList.size() + ")\n");//
 
     }
+
 }
