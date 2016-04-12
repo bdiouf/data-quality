9f70dd511dc90bc5a2c10790512a6c971a8709d5
Sizhao Liu <sizhaoliu@talend.com>
2016-04-12 13:06:41 +0200

    TDQ-11438 implement stream version of reservoir sampling algorithms

---
+++ dataquality-sampling/src/main/java/org/talend/dataquality/sampling/collectors/FastReservoirSampleCollector.java
@@ -0,0 +1,119 @@
+package org.talend.dataquality.sampling.collectors;
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class FastReservoirSampleCollector<T> implements Collector<T, List<T>, List<T>> {
+
+    final Random rand;
+
+    final int nbSamples;
+
+    int count = 0;
+
+    private final int threshold;
+
+    private int g = 0;
+
+    public FastReservoirSampleCollector(int nbSamples, long seed) {
+        this.nbSamples = nbSamples;
+        threshold = 4 * nbSamples;
+        rand = new Random(seed);
+    }
+
+    public FastReservoirSampleCollector(int nbSamples) {
+        this.nbSamples = nbSamples;
+        threshold = 4 * nbSamples;
+        rand = new SecureRandom();
+    }
+
+    private void addIt(final List<T> candidates, T v) {
+
+        if (count < nbSamples) {
+            // for the first n elements.
+            candidates.add(v);
+            count++;
+            return;
+        }
+
+        if (count < threshold) {
+
+            // do reservoir sampling.
+            // rand.nextDouble gets a pseudo random value between 0.0 and 1.0
+            long replace = (long) Math.floor(count * rand.nextDouble());
+            if (replace < nbSamples) {
+                // probability says replace.
+                candidates.set((int) replace, v);
+            }
+            // else keep the current sample reservoir
+        } else {
+            if (g == 0) {
+                // draw gap size (g) from geometric distribution with probability p = R/j
+                double p = (double) nbSamples / count;
+                double u = rand.nextDouble(); // random float > 0 and <= 1
+                g = (int) Math.floor(Math.log10(u) / Math.log10(1 - p));
+
+            } else {
+                g--;
+                if (g == 0) {
+                    int replace = (int) Math.floor(nbSamples * rand.nextDouble());
+                    candidates.set(replace, v);
+                }
+            }
+
+        }
+
+        count++;
+
+    }
+
+    @Override
+    public Supplier<List<T>> supplier() {
+        return ArrayList::new;
+    }
+
+    @Override
+    public BiConsumer<List<T>, T> accumulator() {
+        return this::addIt;
+    }
+
+    @Override
+    public BinaryOperator<List<T>> combiner() {
+        return (left, right) -> {
+            left.addAll(right);
+            return left;
+        };
+    }
+
+    @Override
+    public Set<java.util.stream.Collector.Characteristics> characteristics() {
+        return EnumSet.of(Collector.Characteristics.UNORDERED, Collector.Characteristics.IDENTITY_FINISH);
+    }
+
+    @Override
+    public Function<List<T>, List<T>> finisher() {
+        return (i) -> i;
+    }
+
+    public static void main(String[] args) {
+        Stream<Integer> stream = IntStream.range(0, (int) 5e8).boxed();
+        long begin = System.currentTimeMillis();
+
+        List<Integer> sample = stream.collect(new FastReservoirSampleCollector<>((int) 1e4, 12345678L));
+
+        long end = System.currentTimeMillis();
+
+        System.out.println("duration: " + (end - begin) + " ms");
+    }
+}
---
+++ dataquality-sampling/src/main/java/org/talend/dataquality/sampling/collectors/ReservoirSampleCollector.java
@@ -0,0 +1,84 @@
+package org.talend.dataquality.sampling.collectors;
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class ReservoirSampleCollector<T> implements Collector<T, List<T>, List<T>> {
+
+    final Random rand;
+
+    final int sz;
+
+    int c = 0;
+
+    public ReservoirSampleCollector(int size, long seed) {
+        this.sz = size;
+        rand = new Random(seed);
+    }
+
+    public ReservoirSampleCollector(int size) {
+        this.sz = size;
+        rand = new SecureRandom();
+    }
+
+    private void addIt(final List<T> in, T s) {
+        if (in.size() < sz) {
+            in.add(s);
+        } else {
+            int replaceInIndex = (int) (rand.nextDouble() * (sz + (c++) + 1));
+            if (replaceInIndex < sz) {
+                in.set(replaceInIndex, s);
+            }
+        }
+    }
+
+    @Override
+    public Supplier<List<T>> supplier() {
+        return ArrayList::new;
+    }
+
+    @Override
+    public BiConsumer<List<T>, T> accumulator() {
+        return this::addIt;
+    }
+
+    @Override
+    public BinaryOperator<List<T>> combiner() {
+        return (left, right) -> {
+            left.addAll(right);
+            return left;
+        };
+    }
+
+    @Override
+    public Set<java.util.stream.Collector.Characteristics> characteristics() {
+        return EnumSet.of(Collector.Characteristics.UNORDERED, Collector.Characteristics.IDENTITY_FINISH);
+    }
+
+    @Override
+    public Function<List<T>, List<T>> finisher() {
+        return (i) -> i;
+    }
+
+    public static void main(String[] args) {
+        Stream<Integer> stream = IntStream.range(0, (int) 1e9).boxed();
+        long begin = System.currentTimeMillis();
+
+        List<Integer> sample = stream.collect(new ReservoirSampleCollector<>((int) 1e4, 12345678L));
+
+        long end = System.currentTimeMillis();
+
+        System.out.println("duration: " + (end - begin) + " ms");
+    }
+}
