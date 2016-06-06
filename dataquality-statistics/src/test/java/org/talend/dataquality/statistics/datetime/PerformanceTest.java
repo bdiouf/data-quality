bfd5e46f7193b08796b1dec67680f1aa7a2ea752
Sizhao Liu <sizhaoliu@talend.com>
2016-06-06 12:18:37 +0200

    TDQ-11574 use cpu time instead of absolute time in performance tests

--- dataquality-statistics/src/test/java/org/talend/dataquality/statistics/datetime/PerformanceTest.java
+++ dataquality-statistics/src/test/java/org/talend/dataquality/statistics/datetime/PerformanceTest.java
@@ -16,12 +16,12 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
 import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.io.IOUtils;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,86 +32,60 @@ public class PerformanceTest {
 
     private static final int REPLICATE = 333;
 
-    private static final int INTERVAL_COUNT = 5000;
-
     @Test
-    @Ignore
     public void testIsDate() throws IOException {
 
-        InputStream stream = SystemDateTimePatternManager.class.getResourceAsStream("DateSampleTable.txt");
-        List<String> lines = IOUtils.readLines(stream);
+        final InputStream stream = SystemDateTimePatternManager.class.getResourceAsStream("DateSampleTable.txt");
+        final List<String> lines = IOUtils.readLines(stream);
 
         SystemDateTimePatternManager.isDate("12/02/99");// init DateTimeFormatters
-        Date begin = new Date();
 
-        long currentMilliseconds = begin.getTime();
+        final ThreadMXBean mxBean = ManagementFactory.getThreadMXBean();
+        final long cpuBefore = mxBean.getCurrentThreadCpuTime();
 
-        int count = 0;
         for (int i = 1; i < lines.size(); i++) {
             for (int n = 0; n < REPLICATE; n++) {
-                String line = lines.get(i);
+                final String line = lines.get(i);
                 if (!"".equals(line.trim())) {
-                    String[] sampleLine = line.trim().split("\t");
-                    String sample = sampleLine[0];
+                    final String[] sampleLine = line.trim().split("\t");
+                    final String sample = sampleLine[0];
                     CustomDateTimePatternManager.isDate(sample, Collections.emptyList());
-                    count++;
-                    if (count % INTERVAL_COUNT == 0) {
-                        Date after = new Date();
-                        long difference = after.getTime() - currentMilliseconds;
-                        currentMilliseconds = after.getTime();
-                        LOGGER.debug("count: " + count + "\tinteval: " + difference + "ms");
-                    }
                 }
             }
         }
 
-        Date end = new Date();
-        // Assert count of matches.
-        long difference = end.getTime() - begin.getTime();
+        final long cpuAfter = mxBean.getCurrentThreadCpuTime();
 
-        LOGGER.debug("Total duration IS_DATE on " + count + " samples: " + difference + "ms");
-
-        assertTrue("The method is slower than expected", difference < 4000);
+        final long difference = cpuAfter - cpuBefore;
+        assertTrue("The method isDate() is slower than expected. Actual CPU time spent: " + difference, difference < 1.3e9);
     }
 
     @Test
-    @Ignore
     public void testGetPatterns() throws IOException {
 
-        InputStream stream = SystemDateTimePatternManager.class.getResourceAsStream("DateSampleTable.txt");
-        List<String> lines = IOUtils.readLines(stream);
+        final InputStream stream = SystemDateTimePatternManager.class.getResourceAsStream("DateSampleTable.txt");
+        final List<String> lines = IOUtils.readLines(stream);
 
         SystemDateTimePatternManager.datePatternReplace("12/02/99");// init DateTimeFormatters
-        Date begin = new Date();
 
-        long currentMilliseconds = begin.getTime();
+        final ThreadMXBean mxBean = ManagementFactory.getThreadMXBean();
+        final long cpuBefore = mxBean.getCurrentThreadCpuTime();
 
-        int count = 0;
         for (int i = 1; i < lines.size(); i++) {
             for (int n = 0; n < REPLICATE; n++) {
-                String line = lines.get(i);
+                final String line = lines.get(i);
                 if (!"".equals(line.trim())) {
-                    String[] sampleLine = line.trim().split("\t");
-                    String sample = sampleLine[0];
+                    final String[] sampleLine = line.trim().split("\t");
+                    final String sample = sampleLine[0];
                     SystemDateTimePatternManager.datePatternReplace(sample);
-                    count++;
-                    if (count % INTERVAL_COUNT == 0) {
-                        Date after = new Date();
-                        long difference = after.getTime() - currentMilliseconds;
-                        currentMilliseconds = after.getTime();
-                        LOGGER.debug("count: " + count + "\tinteval: " + difference + "ms");
-                    }
                 }
             }
         }
 
-        Date end = new Date();
-        // Assert count of matches.
-        long difference = end.getTime() - begin.getTime();
+        final long cpuAfter = mxBean.getCurrentThreadCpuTime();
 
-        LOGGER.debug("Total duration GET_PATTERNS on " + count + " samples: " + difference + "ms");
-
-        assertTrue("The method is slower than expected", difference < 5000);
+        final long difference = cpuAfter - cpuBefore;
+        assertTrue("The method getPatterns() is slower than expected. Actual CPU time spent: " + difference, difference < 1.4e9);
     }
 
 }
