9263a615a60a0942a968000f48fbc38a2b1ffc4f
Damien Prot <dprot@talend.com>
2016-06-15 09:08:41 +0200

    Cleaning using Sonar

--- dataquality-sampling/src/main/java/org/talend/dataquality/datamasking/functions/GenerateUniqueSsnIndia.java
+++ dataquality-sampling/src/main/java/org/talend/dataquality/datamasking/functions/GenerateUniqueSsnIndia.java
@@ -29,18 +29,18 @@ public class GenerateUniqueSsnIndia extends AbstractGenerateUniqueSsn {
     private static final long serialVersionUID = 4514471121590047091L;
 
     // The multiplication table (for checksum)
-    final static int[][] D = new int[][] { { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, { 1, 2, 3, 4, 0, 6, 7, 8, 9, 5 },
+    private static final int[][] D = new int[][] { { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, { 1, 2, 3, 4, 0, 6, 7, 8, 9, 5 },
             { 2, 3, 4, 0, 1, 7, 8, 9, 5, 6 }, { 3, 4, 0, 1, 2, 8, 9, 5, 6, 7 }, { 4, 0, 1, 2, 3, 9, 5, 6, 7, 8 },
             { 5, 9, 8, 7, 6, 0, 4, 3, 2, 1 }, { 6, 5, 9, 8, 7, 1, 0, 4, 3, 2 }, { 7, 6, 5, 9, 8, 2, 1, 0, 4, 3 },
             { 8, 7, 6, 5, 9, 3, 2, 1, 0, 4 }, { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 } };
 
     // The permutation table (for checksum)
-    final static int[][] P = new int[][] { { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, { 1, 5, 7, 6, 2, 8, 3, 0, 9, 4 },
+    private static final int[][] P = new int[][] { { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, { 1, 5, 7, 6, 2, 8, 3, 0, 9, 4 },
             { 5, 8, 0, 3, 7, 9, 6, 1, 4, 2 }, { 8, 9, 1, 6, 0, 4, 3, 5, 2, 7 }, { 9, 4, 5, 3, 1, 2, 6, 8, 7, 0 },
             { 4, 2, 8, 6, 5, 7, 3, 9, 0, 1 }, { 2, 7, 9, 3, 8, 0, 6, 4, 1, 5 }, { 7, 0, 4, 6, 9, 1, 3, 2, 5, 8 } };
 
     // The inverse table (for checksum)
-    final static int[] INV = { 0, 4, 3, 2, 1, 5, 6, 7, 8, 9 };
+    private static final int[] INV = { 0, 4, 3, 2, 1, 5, 6, 7, 8, 9 };
 
     @Override
     protected StringBuilder doValidGenerateMaskedField(String str) {
@@ -89,7 +89,7 @@ public class GenerateUniqueSsnIndia extends AbstractGenerateUniqueSsn {
         int[] myArray = stringToReversedIntArray(string);
 
         for (int i = 0; i < myArray.length; i++) {
-            c = D[c][P[((i + 1) % 8)][myArray[i]]];
+            c = D[c][P[(i + 1) % 8][myArray[i]]];
         }
 
         return Integer.toString(INV[c]);
