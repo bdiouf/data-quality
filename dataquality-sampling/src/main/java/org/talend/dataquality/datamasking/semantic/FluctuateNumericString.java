3a17fa2ec7bd842f69f19883577deaee9e9fbdc5
xqliu-talend <xqliu@talend.com>
2016-07-22 16:34:33 +0800

    TDQ-12269 resolve the format prolbem

--- dataquality-sampling/src/main/java/org/talend/dataquality/datamasking/semantic/FluctuateNumericString.java
+++ dataquality-sampling/src/main/java/org/talend/dataquality/datamasking/semantic/FluctuateNumericString.java
@@ -35,8 +35,7 @@ public class FluctuateNumericString extends NumericVariance<String> {
             if (patternInteger.matcher(input).matches()) {
                 BigDecimal bigDecimal = new BigDecimal(input);
                 if (bigDecimal.abs().compareTo(new BigDecimal(Long.MAX_VALUE)) > 0) {
-                    final BigDecimal result = bigDecimal.multiply(BigDecimal.valueOf(rateToApply + 100)).divide(
-                            new BigDecimal(100));
+                    final BigDecimal result = bigDecimal.multiply(new BigDecimal(rateToApply + 100)).divide(new BigDecimal(100));
                     return result.setScale(0, RoundingMode.HALF_UP).toString();
                 } else {
                     final long result = Math.round(Long.valueOf(input) * (rateToApply + 100) / 100);
@@ -47,7 +46,7 @@ public class FluctuateNumericString extends NumericVariance<String> {
                     BigDecimal bigDecimal = BigDecimalParser.toBigDecimal(input);
                     final int decimalLength = getDecimalPrecision(input);
                     if (bigDecimal.abs().compareTo(new BigDecimal(Long.MAX_VALUE)) > 0) {
-                        final BigDecimal result = bigDecimal.multiply(BigDecimal.valueOf(rateToApply + 100))
+                        final BigDecimal result = bigDecimal.multiply(new BigDecimal(rateToApply + 100))
                                 .divide(new BigDecimal(100));
                         if (input.contains("e") || input.contains("E")) {
                             return String.valueOf(result.setScale(decimalLength, RoundingMode.HALF_UP).doubleValue());
