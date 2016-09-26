39a1d6b647a42301f2fb1c1da8c1a443df2e6347
Sizhao Liu <sizhaoliu@talend.com>
2016-09-26 13:25:18 +0200

    TDQ-12506 import commons-validator packages for phone number validation

--- dataquality-semantic/src/main/java/org/talend/dataquality/semantic/validator/impl/IBANValidator.java
+++ dataquality-semantic/src/main/java/org/talend/dataquality/semantic/validator/impl/IBANValidator.java
@@ -1,6 +1,6 @@
 package org.talend.dataquality.semantic.validator.impl;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.talend.dataquality.semantic.validator.ISemanticValidator;
 
 /**
