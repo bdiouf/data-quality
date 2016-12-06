ff5d5822b80abe9204a10afb06135490390d04bd
xqliu-talend <xqliu@talend.com>
2016-12-06 15:14:01 +0800

    TDQ-12507 Reduce technical debt TDQ-12656 Improve code quality of email library

--- dataquality-email/src/main/java/org/talend/dataquality/email/Constant.java
+++ dataquality-email/src/main/java/org/talend/dataquality/email/Constant.java
@@ -12,8 +12,6 @@
 // ============================================================================
 package org.talend.dataquality.email;
 
-import java.util.Arrays;
-
 /**
  * created by talend on 2014年12月29日 Detailled comment
  *
@@ -24,21 +22,13 @@ public final class Constant {
         // nothing here
     }
 
-    public static final char LEFT_BRACKET = '<'; //$NON-NLS-1$
+    public static final char LEFT_BRACKET = '<'; // $NON-NLS-1$
 
-    public static final char RIGHT_END = '>'; //$NON-NLS-1$
+    public static final char RIGHT_END = '>'; // $NON-NLS-1$
 
-    public static final char DOT = '.'; //$NON-NLS-1$
+    public static final char DOT = '.'; // $NON-NLS-1$
 
-    public static final char SLASH = '\\'; //$NON-NLS-1$
+    public static final char SLASH = '\\'; // $NON-NLS-1$
 
     public static final char AT = '@';
-
-    // MOD sizhaoliu
-    // SLASH must be handled at the first place, because there may be fake slashes in front of the other special chars
-    private static final String[] SPECIAL_CHARS = { "\\", "[", "]", "?", "*", "+", "{", "}", "^", "-", "&", "$", "." }; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$ //$NON-NLS-8$ //$NON-NLS-9$ //$NON-NLS-10$ //$NON-NLS-11$ //$NON-NLS-12$ //$NON-NLS-13$
-
-    public static String[] getSpecialChars() {
-        return Arrays.copyOf(SPECIAL_CHARS, SPECIAL_CHARS.length);
-    }
 }
